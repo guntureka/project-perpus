1bf7af44c47cd71560d0040742be24202583d97e
Varian Avila Faldi <varian236@gmail.com>
2023-11-08 17:59:36 +0700

    add payment and user show page

--- app/Views/pages/dashboard/user/show_user.php
+++ app/Views/pages/dashboard/user/show_user.php
@@ -2,7 +2,12 @@
 
 <?= $this->section('content'); ?>
 <div class="mx-auto mt-5" style="width: 90%">
-    <h1 class="text-3xl font-bold text-secondary-1">List Peminjam Buku</h1>
+    <h1 class="text-3xl font-bold text-secondary-1">List User</h1>
+    <a href="/user/add">
+        <button class="mt-4 py-2 px-4 bg-green text-center rounded-4 text-white">
+            Add New User
+        </button>
+    </a>
     <div class="w-full rounded-4 overflow-hidden mt-2">
         <table id="table" class="w-full bg-secondary-1 text-primary-1">
             <thead class="text-left">
@@ -23,21 +28,21 @@
                   </td>
                   <td class="text-green">TRUE</td>
                   <td>
-                    <a href="/book/edit/">
+                    <a href="/user/edit/">
                         <button class="py-2 px-4 bg-tertiary-1 text-center rounded-4 text-white">
                             edit
                         </button>
                     </a>
-                    <a href="/book/delete/">
+                    <a href="/user/delete/">
                         <button class="mt-2 py-2 px-4 bg-red text-center rounded-4 text-white">
                             delete
                         </button>
                     </a>
                 </td> 
               </tr>
-                <!-- <tr>
-                    <td colspan="9">No Data</td>
-                </tr> -->
+                <tr>
+                    <td colspan="9" class="text-center">No Data</td>
+                </tr>
             </tbody>
         </table>
     </div>
